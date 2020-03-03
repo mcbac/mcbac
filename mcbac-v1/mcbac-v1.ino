c37a9bd855565848d80155a7c100ab168cc7490a
WestleyK <westley@sylabs.io>
2020-03-03 15:52:55 -0800

    Updated code; added rotary encoder

--- mcbac-v1/mcbac-v1.ino
+++ mcbac-v1/mcbac-v1.ino
@@ -24,6 +24,8 @@
 #define buttonUp A2
 #define buttonDown A3
 
+#define R_CW 2
+#define R_CC 3
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
@@ -32,21 +34,33 @@ void setup() {
   pinMode(buttonBack, INPUT_PULLUP);
   pinMode(buttonUp, INPUT_PULLUP);
   pinMode(buttonDown, INPUT_PULLUP);
-  
+
+  pinMode(R_CW, INPUT_PULLUP);
+  pinMode(R_CC, INPUT_PULLUP);
+
   // Initialize the LCD
   lcd.begin();
+
+  attachInterrupt(0, cursorCW, RISING);
+  attachInterrupt(1, cursorCC, RISING);
+
+  // For debugging
+  Serial.begin(9600);
 
   // Turn on the blacklight and print a message.
   lcd.backlight();
   lcd.print("MCBAC V1.0.0");
-  delay(3000);
+  delay(2000);
 }
 
 // When 1; update the LCD
 int updateDisplay = 1;
 
-int curser;
-int curserRow;
+// For the rotory encoder
+volatile int lastCW;
+volatile int lastCC;
+volatile int cursorCount;
+volatile int cursorRow;
 
 int enterB = 0;
 int backB = 0;
@@ -58,6 +72,37 @@ void resetAllButtons() {
   backB = 0;
   upB = 0;
   downB = 0;
+}
+
+void cursorCW(){
+  cli();
+  // Read pins D, and only keep pins 2 and 3
+  byte reading = PIND & B00001100;
+  if (reading == B00001100 && lastCW == 1) {
+    cursorCount++;
+    updateDisplay = 1;
+    lastCW = 0;
+    lastCC = 0;
+  } else if (reading == B00001000) {
+    lastCC = 1;
+  }
+  sei();
+}
+
+void cursorCC(){
+  cli();
+  // Read pins D, and only keep pins 2 and 3
+  byte reading = PIND & B00001100;
+  if (reading == B00001100 && lastCC == 1) {
+    cursorCount--;
+    updateDisplay = 1;
+    if (cursorCount <= 0) cursorCount = 0;
+    lastCC = 0;
+    lastCW = 0;
+  } else if (reading == B00000100) {
+    lastCW = 1;
+  }
+  sei();
 }
 
 int checkButtons() {
@@ -77,16 +122,6 @@ int checkButtons() {
   // Maybe dont do this:
   resetAllButtons();
   return 0;
-}
-
-void curserUp() {  
-  curser++;
-  updateDisplay = 1;
-}
-
-void curserDown() {  
-  curser--;
-  updateDisplay = 1;
 }
 
 // The function that charges a battery
@@ -116,7 +151,6 @@ void chargeLi() {
   }
 }
 
-
 void chargeBattery(int prog) {
   if (prog == 1) {
     chargeLi();
@@ -138,25 +172,53 @@ int batteryProg;
 
 void mainMenu() {
   if (updateDisplay == 1) {
-    if (curser == 0) {
+    if (cursorCount == 0) {
       lcd.clear();
       lcd.setCursor(3, 2);
       lcd.print("Lithium");
       lcd.setCursor(3, 2);
       lcd.print("batteries");
       batteryProg = 1;
-   } else if (curser == 1) {
+      Serial.println("Lithium battery");
+      updateDisplay = 0;
+   } else if (cursorCount == 1) {
       lcd.clear();
       lcd.setCursor(3, 3);
       lcd.print("Nickel");
       lcd.setCursor(3, 3);
       lcd.print("batteries");
       batteryProg = 2;
+      Serial.println("Nickel battery");
+      updateDisplay = 0;
+   } else if (cursorCount == 2) {
+      lcd.clear();
+      lcd.setCursor(3, 3);
+      lcd.print("Lead acid");
+      lcd.setCursor(3, 3);
+      lcd.print("batteries");
+      batteryProg = 3;
+      Serial.println("Lead acid battery");
+      updateDisplay = 0;
+    } else if (cursorCount == 3) {
+      lcd.clear();
+      lcd.setCursor(3, 3);
+      lcd.print("Power");
+      lcd.setCursor(3, 3);
+      lcd.print("Supply");
+      batteryProg = 4;
+      Serial.println("Power Supply");
+      updateDisplay = 0;
+    } else if (cursorCount == 4) {
+      lcd.clear();
+      lcd.setCursor(3, 3);
+      lcd.print("DC Load");
+      batteryProg = 5;
+      Serial.println("DC Load");
+      updateDisplay = 0;
     } else {
-      curser = 0;
+      cursorCount = 0;
       updateDisplay = 1;
     }
-    updateDisplay = 0;
   }
 
   if (enterB == 1) {
